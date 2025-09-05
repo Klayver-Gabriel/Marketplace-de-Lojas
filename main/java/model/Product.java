95cfd9628ebebc9e59ce65a91fcab62259bfffc1
Klayver <jondopicoblauversaopicoblau@gmail.com>
2025-09-05 19:56:17 -0300

    Creating-the-first-instances-of-the-project

---
+++ main/java/model/Product.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+    private long quantity;
+
+    Product(String name, String description, double price, long quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public long getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+    @Override
+    public String toString() {
+        return "Produto: " + this.name + "\n" + "Descrição" + this.description + "\n" + "Quantidade: " + this.quantity;
+
+    }
+}
